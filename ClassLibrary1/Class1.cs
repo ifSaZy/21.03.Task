ceb17496f0d8b7cf2a8352720de8eeff10bffd31
ifSaZy <65533483+ifSaZy@users.noreply.github.com>
2022-03-21 09:30:25 +0300

    Task
    task

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class KU
+    {
+        /// <summary>
+        /// Вычисление квадратного уравнения
+        /// </summary>
+        /// <param name="a">коэффициент</param>
+        /// <param name="b">коэффициент</param>
+        /// <param name="c">коэффициент</param>
+        /// <param name="d">дискриминант</param>
+        /// <param name="x1">1 корень</param>
+        /// <param name="x2">2 корень</param>
+        /// <param name="message">сообщение</param>
+        public static void KvUr(
+            double a, double b, double c,
+            out double d, out double x1, out double x2,
+            out string message)
+        {
+            d = x1 = x2 = double.NaN;
+
+            if (a == 0)
+            {
+                LnUr(b, c, out x1, out message);
+            }
+            else
+            {
+                d = b * b - 4 * a * c;
+                if (d > 0)
+                {
+                    x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    message = "Дискриминант больше нуля, 2 корня.";
+                }
+                else if (d == 0)
+                {
+                    x1 = -b / (2 * a);
+                    message = "Дискриминант равен нулю, 1 корня.";
+                }
+                else
+                {
+                    message = "Дискриминант меньше нуля, нет корней.";
+                }
+            }
+
+        }
+
+        public static void LnUr(double k, double b,
+            out double x, out string message)
+        {
+            if (k == 0)
+            {
+                x = double.NaN;
+                if (b == 0) message = "Прямая совпадает с осью x, ответ R";
+                else message = "Нет решений";
+            }
+            else
+            {
+                x = -b / k;
+                message = null;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class KvUrTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double x1_exp = double.NaN, x2_exp = double.NaN, d_exp = double.NaN;
+            string message_exp = "Прямая совпадает с осью x, ответ R";
+
+            double x1_act, x2_act, d_act;
+            string message_act;
+
+            KU.KvUr(0, 0, 0, out d_act, out x1_act, out x2_act, out message_act);
+
+            Assert.AreEqual(d_exp, d_act);
+            Assert.AreEqual(x1_exp, x1_act);
+            Assert.AreEqual(x2_exp, x2_act);
+            Assert.AreEqual(message_exp, message_act);
+        }
+    }
+}
